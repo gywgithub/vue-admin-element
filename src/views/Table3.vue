eafe09fe5134120d43087aa6cef690e5738b2668
gywgithub <qingyi_w@outlook.com>
2022-07-25 16:40:59 +0800

    clear code

--- src/views/Table3.vue
+++ src/views/Table3.vue
@@ -1,69 +1,81 @@
 <template>
-  <div class="table1">
-    <div>表格合并一 —— Element UI 自带功能合并 </div>
+  <div style="padding: 10px">
+    <h2>表格合并</h2>
+    <h4>1. Element UI 自带功能合并</h4>
     <el-table
-    border
+      border
       ref="multipleTable"
       :data="tableData"
       tooltip-effect="dark"
-      style="width: 800px;margin: 20px;"
+      style="width: 800px; margin: 10px 0"
       @selection-change="handleSelectionChange"
+      :span-method="objectSpanMethod"
     >
-    <el-table-column
-     type="selection"
-     width="55">
-   </el-table-column>
-   <el-table-column label="ID" width="120">
-     <template slot-scope="scope">{{ scope.row.id }}</template>
-   </el-table-column>
-      <el-table-column label="日期" width="120">
-        <template slot-scope="scope">{{ scope.row.date }}</template>
+      <el-table-column type="selection" width="55"> </el-table-column>
+      <el-table-column label="ID" width="120">
+        <template slot-scope="scope">{{ scope.row.id }}</template>
       </el-table-column>
-      <el-table-column label="姓名" width="120">
-        <template slot-scope="scope">
-          <div v-if="scope.row.back" style="width: 100%;height: 100%;margin: 0;padding: 0;background-color: #ffff00;">{{ scope.row.name }}
-            </div>
-            <div v-else>{{ scope.row.name }}
-            </div>
-          </template>
-      </el-table-column>
-      <el-table-column prop="address" label="地址" show-overflow-tooltip>
+      <el-table-column prop="date" label="日期" width="120"> </el-table-column>
+      <el-table-column prop="name" label="姓名" width="120"> </el-table-column>
+      <el-table-column prop="address" label="地址">
         <template slot-scope="scope">
           <div>
             {{ scope.row.address }}
-            </div>
-          </template>
+          </div>
+        </template>
       </el-table-column>
     </el-table>
 
-    <div>表格合并二 —— 自定义表格列</div>
+    <h4>2. 自定义表格列</h4>
+
     <div class="table2">
-    <el-table
-    border
-      ref="multipleTable"
-      :data="tableData"
-      tooltip-effect="dark"
-      style="width: 800px;margin: 20px;"
-      @selection-change="handleSelectionChange"
-    >
-    <el-table-column
-     type="selection"
-     width="55">
-   </el-table-column>
-   <el-table-column label="ID" width="120">
-     <template slot-scope="scope">{{ scope.row.id }}</template>
-   </el-table-column>
-      <el-table-column label="日期" width="120">
-        <template slot-scope="scope">{{ scope.row.date }}</template>
-      </el-table-column>
-      <el-table-column label="姓名" width="120">
-        <template slot-scope="scope">
-          <div>{{ scope.row.name }}</div>
-        </template>
-      </el-table-column>
-      <el-table-column prop="address" label="地址" class-name="cell-control-type">
-      </el-table-column>
-    </el-table>
+      <el-table
+        border
+        ref="multipleTable"
+        :data="tableData2"
+        tooltip-effect="dark"
+        style="width: 800px; margin: 10px 0"
+        @selection-change="handleSelectionChange"
+      >
+        <el-table-column type="selection" width="55"> </el-table-column>
+        <el-table-column label="ID" width="120">
+          <template slot-scope="scope">{{ scope.row.id }}</template>
+        </el-table-column>
+        <el-table-column label="日期" width="120">
+          <template slot-scope="scope">{{ scope.row.date }}</template>
+        </el-table-column>
+        <el-table-column label="姓名" width="120">
+          <template slot-scope="scope">
+            <div>{{ scope.row.name }}</div>
+          </template>
+        </el-table-column>
+        <el-table-column
+          label="地址"
+          class-name="cell-control-type"
+          label-class-name="cell-control-title"
+        >
+          <template slot-scope="scope">
+            <div
+              style=""
+              v-for="(item, index) in scope.row.address"
+              :key="index"
+            >
+              <div
+                v-if="index === scope.row.address.length - 1"
+                style="padding: 10px"
+              >
+                {{ item }}
+              </div>
+              <div
+                v-else
+                style="border-bottom: 1px solid #ebeef5; padding: 10px"
+              >
+                {{ item }}
+              </div>
+            </div>
+          </template>
+        </el-table-column>
+      </el-table>
     </div>
   </div>
 </template>
@@ -80,45 +92,78 @@ export default {
           id: '1',
           date: '2016-05-03',
           name: '张三',
-          address: '上海市普陀区金沙江路 1518 '
+          address: '家庭地址：上海市普陀区金沙江路'
+        },
+        {
+          id: '1',
+          date: '2016-05-03',
+          name: '张三',
+          address: '公司地址：北京市朝阳区金台路'
         },
         {
           id: '2',
           date: '2016-05-02',
           name: '王小虎',
           address: '上海市普陀区金沙江路 1518 '
+        }
+      ],
+      multipleSelection: [],
+      tableData2: [
+        {
+          id: '1',
+          date: '2016-05-03',
+          name: '张三',
+          address: ['家庭地址：上海市普陀区金沙江路', '公司地址：北京市朝阳区金台路']
+        },
+        {
+          id: '2',
+          date: '2016-05-02',
+          name: '王小虎',
+          address: ['上海市普陀区金沙江路 1518 ']
         },
         {
           id: '3',
-          date: '2016-05-04',
-          name: '张三',
-          address: '上海市普陀区金沙江路 1518 '
-        },
-        {
-          id: '4',
-          date: '2016-05-01',
-          name: '张三',
-          address: '上海市普陀区金沙江路 1518 '
+          date: '2016-05-02',
+          name: '李四',
+          address: ['广州市xxx区']
         }
-      ],
-      multipleSelection: []
+      ]
     }
   },
   methods: {
+    objectSpanMethod ({ row, column, rowIndex, columnIndex }) {
+      if (columnIndex === 0) {
+        if (rowIndex % 2 === 0) {
+          return {
+            rowspan: 2,
+            colspan: 1
+          }
+        } else {
+          return {
+            rowspan: 0,
+            colspan: 0
+          }
+        }
+      }
+    },
     handleSelectionChange (val) {
       this.multipleSelection = val
+      console.log(val)
     }
   }
 }
 </script>
 <style>
 .cell-control-type {
-  /* padding: 0 10px !important; */
-  border: 2px solid red;
+  padding: 0 0 !important;
 }
 
-.table2 .el-table .cell {
-  /* padding-right: 10px !important;
-  padding-left: 10px !important; */
+.cell-control-title {
+  margin-left: 10px;
 }
+
+.cell-control-type .cell {
+  padding: 0 0 !important;
+}
+
 </style>
